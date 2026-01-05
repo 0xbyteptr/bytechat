f269a444e87abb652492bd38d671f8c447f7cfd0
BytePtr <100956266+0xbyteptr@users.noreply.github.com>
2026-01-05 14:55:43 +0100

    fixy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig(async () => {
   const { svelte } = await import('@sveltejs/vite-plugin-svelte')
   return {
+    base: './',
     plugins: [
       svelte(),
       VitePWA({
@@ -15,18 +16,7 @@ export default defineConfig(async () => {
           theme_color: '#1e1e2e',
           background_color: '#1e1e2e',
           display: 'standalone',
-          icons: [
-            {
-              src: 'icon-192.png',
-              sizes: '192x192',
-              type: 'image/png'
-            },
-            {
-              src: 'icon-512.png',
-              sizes: '512x512',
-              type: 'image/png'
-            }
-          ]
+          icons: []
         }
       })
     ],
