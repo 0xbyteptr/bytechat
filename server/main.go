55e58a2c81a87f0d92344494c0dfbb90637c4ecd
BytePtr <100956266+0xbyteptr@users.noreply.github.com>
2026-01-05 13:48:29 +0100

    modified:   server/main.go

--- server/main.go
+++ server/main.go
@@ -98,12 +98,13 @@ func (c *client) send(v interface{}) error {
 
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("CORS Middleware: %s %s from %s\n", r.Method, r.URL.Path, r.RemoteAddr)
+		// Set CORS headers for every request, including errors
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 		next(w, r)
