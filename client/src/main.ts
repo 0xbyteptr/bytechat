6481f464cb15f35622e9a3e8f3ff0632789dacb5
BytePtr <100956266+0xbyteptr@users.noreply.github.com>
2026-01-06 05:10:07 +0100

    my moms kinda homeless (speed insights)

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,10 @@
 import './styles.css'
 import App from './App.svelte'
 import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 
 inject()
+injectSpeedInsights()
 
 const app = new App({
   target: document.getElementById('app')!,
